7aece25da57459834ea9a49769d709c173a28538
pjs102793 <pjs102793>
2023-08-10 08:31:24 +0000

    Black format 적용

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 import time
-from cv2 import imshow, waitKey, imwrite
+import cv2
 
 import normalSpeed
 
@@ -11,7 +11,7 @@ def norm2bgr(norm):
     return norm
 
 
-def depth2show(depth, norm_type='max'):
+def depth2show(depth, norm_type="max"):
     show_depth = (depth / depth.max() * 256).astype("uint8")
     return show_depth
 
@@ -36,23 +36,26 @@ if __name__ == "__main__":
         - y is down (to align to the actual pixel coordinates used in digital images)
         - right-handed: positive z look-at direction
     """
-    normals_map_out = normalSpeed.depth_normal(depth, fx, fy, k_size, distance_threshold, difference_threshold, point_into_surface)
+    normals_map_out = normalSpeed.depth_normal(
+        depth,
+        fx,
+        fy,
+        k_size,
+        distance_threshold,
+        difference_threshold,
+        point_into_surface,
+    )
 
     # If the normal point out the surface, the z of normal should be negetive.
-    print("normals_map_out z mean:",  normals_map_out[:, :, 2].mean())
+    print("normals_map_out z mean:", normals_map_out[:, :, 2].mean())
     # If the normal point into the surface, the z of normal should be positive, as z of depth.
-    normals_map_in = normalSpeed.depth_normal(depth, fx, fy, k_size, distance_threshold, difference_threshold, True)
-    print("normals_map_in z mean:",  normals_map_in[:, :, 2].mean())
+    normals_map_in = normalSpeed.depth_normal(
+        depth, fx, fy, k_size, distance_threshold, difference_threshold, True
+    )
+    print("normals_map_in z mean:", normals_map_in[:, :, 2].mean())
 
-    imshow('norm_out', norm2bgr(normals_map_out))
-    imwrite("examplePicture/normal_out.jpg", norm2bgr(normals_map_out))
+    cv2.imwrite("examplePicture/normal_out.jpg", norm2bgr(normals_map_out))
 
-    imshow('norm_in', norm2bgr(normals_map_in))
-    imwrite("examplePicture/normal_in.jpg", norm2bgr(normals_map_in))
+    cv2.imwrite("examplePicture/normal_in.jpg", norm2bgr(normals_map_in))
 
-    imshow('depth', depth2show(depth))
-    imwrite('examplePicture/depth_view.jpg', depth2show(depth))
-
-    cmd = waitKey(0)
-    if cmd == ord('q'):
-        exit()
+    cv2.imwrite("examplePicture/depth_view.jpg", depth2show(depth))
